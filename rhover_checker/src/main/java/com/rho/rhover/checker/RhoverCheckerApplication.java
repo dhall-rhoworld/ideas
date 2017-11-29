0db6257003a158f2121a3a0e5a572ea4b4e90202
David Hall <david_hal@rhoworld.com>
2017-11-29 09:28:18 -0500

    Initial checkin of rhover checker

---
+++ rhover_checker/src/main/java/com/rho/rhover/checker/RhoverCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.rho.rhover.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhoverCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhoverCheckerApplication.class, args);
+	}
+}
