ad53cc709188a5a477e4f903ea7a8fd26bd017f5
David Hall <david_hal@rhoworld.com>
2018-02-22 09:08:01 -0500

    Changed home page to be more like a dashboard

---
+++ rhover_web/src/main/java/com/rho/rhover/web/controller/DataController.java
@@ -0,0 +1,22 @@
+package com.rho.rhover.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rho.rhover.web.dto.AnomalySummaryBuilder;
+
+@Controller
+@RequestMapping("/")
+public class DataController {
+	
+	@Autowired
+	private AnomalySummaryBuilder anomalySummaryBuilder;
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("summaries", anomalySummaryBuilder.getStudySummaries());
+		return "home";
+	}
+}
