7ce6d60598af6179a197286d2ae4b4348cf3bae8
David Hall <david_hal@rhoworld.com>
2018-03-09 16:18:55 -0500

    Changes so that rhover_checker can build on server

--- rhover_web/src/main/java/com/rho/rhover/web/WebSecurityConfig.java
+++ rhover_web/src/main/java/com/rho/rhover/web/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.rho.rhover.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,8 +30,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("dhall").password("dhall").roles("USER");
+		InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> builder =
+				auth.inMemoryAuthentication();
+        builder.withUser("dhall").password("dhall").roles("USER");
+        builder.withUser("hwildasi").password("abcrhover123").roles("USER");
+        builder.withUser("jguild").password("rhoverabc123").roles("USER");
+        builder.withUser("kcho").password("123abcrhover").roles("USER");
     }
 }
